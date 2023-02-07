fa63ade5329eaede23b8cd43a0444c397976cadf
m-khalouzadeh <m.khalouzadeh88@gmail.com>
2023-02-07 20:13:46 +0330

    pre

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -3,84 +3,84 @@ import "./calculator_style.css";
 
 const MycalCulator = () => {
 
-    const [op,setop]=useState("");
-    const[num1,setnum1]=useState("");
-    const[num2,setnum2]=useState("");
+  const [op, setop] = useState("");
+  const [num1, setnum1] = useState("");
+  const [num2, setnum2] = useState("");
 
-    const[finalnum1,setfinalnum1]=useState("");
-    const[finalnum2,setfinalnum2]=useState("");
+  
 
-    const number_click=(e)=>{
-      setnum1(num1+''+e.target.value);
-      
+  const onNumberClick = (e) => {
+    setnum1(num1 + '' + e.target.value);
+
+  }
+
+  const onOpClick = (e) => {
+    if (num1 == '') return
+    if (num2 != '') {
+      let c = compute();
+      setnum2(c);
+    }
+    else {
+      setnum2(num1);
+    }
+    setnum1("");
+    setop(e.target.value);
+  }
+
+  const equal_click = (e) => {
+
+    setnum1(compute());
+    setnum2("");
+    setop('');
+
+  }
+
+  const compute = (e) => {
+    let x = parseInt(num1);
+    let y = parseInt(num2)
+    switch (op) {
+      case '+': return x + y; break;
+      case '-': return y - x; break;
+      case '*': return x * y; break;
+      case '/': return y / x; break;
 
     }
 
-    const op_click=(e)=>{
-      if(num1=='') return
-      if(num2!='')
-      {
-        let c=compute();
-        setnum2(c);
-      }
-      else
-      {
-        setnum2(num1);
-      }
-        
-        setnum1("");
-        setop(e.target.value);
-  
-      }
-
-      const equal_click=(e)=>{
-      
-        setnum1(compute());
-        setnum2("");
-       setop('');
-  
-      }
-
-      const compute=(e)=>{
-      let x=parseInt(num1);
-      let y=parseInt(num2)
-        switch(op)
-      {
-        case '+': return x+y; break;
-        case '-': return y-x; break;
-        case '*': return x*y; break;
-        case '/': return y/x; break;
-
-      }
-  
-      }
+  }
 
 
 
   return (
-    <div>
-      <div className="mybox">
-        <div style={{display:"inline-grid" , border:"1px"}}>
-        <input className="myinput" value={num2}></input>
-        <input className="myinput" value={num1}></input>
-        </div>
-        <br></br><bt></bt>
-        <button className="mybtn" value={1} onClick={number_click}>1</button>
-        <button className="mybtn" value={2} onClick={number_click}>2</button>
-        <button className="mybtn" value={3} onClick={number_click}>3</button>
-        <button className="mybtn" value={'+'} onClick={op_click}>+</button>
-        <br></br>
+    <div style={{ margin: "auto", width: "300px", marginTop: "50px" }}>
+      <span style={{border:"1px solid" , width: "126px"}}>
+        <input className='myinput' value={num2}></input>
+        
+        <input className='myinput' value={num1}></input>
+      </span>
+      <span>
+        <button className='mybtn' id="b1" value={1} onClick={onNumberClick}>1</button>
+        <button className='mybtn' id="b2" value={2} onClick={onNumberClick}>2</button>
+        <button className='mybtn' id="b3" value={3} onClick={onNumberClick}>3</button>
+        <button className='mybtn opbtn' id="plus" value={'+'} onClick={onOpClick}>+</button>
+      </span>
+      <span>
+        <button className='mybtn' id="b4" value={4} onClick={onNumberClick}>4</button>
+        <button className='mybtn' id="b5" value={5} onClick={onNumberClick}>5</button>
+        <button className='mybtn' id="b6" value={6} onClick={onNumberClick}>6</button>
+        <button className='mybtn opbtn' id="mines" value={"-"} onClick={onOpClick}>-</button>
+      </span>
+      <span>
+        <button className='mybtn' id="b7" value={7} onClick={onNumberClick}>7</button>
+        <button className='mybtn' id="b8" value={8} onClick={onNumberClick}>8</button>
+        <button className='mybtn' id="b9" value={9} onClick={onNumberClick}>9</button>
+        <button className='mybtn opbtn' id="mult" value={"*"} onClick={onOpClick}>*</button>
+      </span>
+      <span>
+        <button className='mybtn' id="b9" value={9} onClick={onNumberClick}>0</button>
+        <button className='btn_equal' id="eq" value={"="} onClick={equal_click}>=</button>
+        <button className='mybtn opbtn' id="divide" value={"/"} onClick={onOpClick}>/</button>
 
-        <button className="mybtn" value={4} onClick={number_click}>4</button>
-        <button className="mybtn" value={5} onClick={number_click}>5</button>
-        <button className="mybtn" value={6} onClick={number_click}>6</button>
-        <button className="mybtn" value={'-'} onClick={op_click}>-</button>
-        <br></br>
-        <button className="mybtn" value={7} onClick={number_click}>7</button>
-        <button className="mybtn" value={8} onClick={number_click}>8</button>
-        <button className="mybtn" value={9} onClick={number_click}>9</button>
-        <button className="mybtn" value={'='} onClick={equal_click}>=</button>
-      </div>
+      </span>
     </div>
   );
 };
